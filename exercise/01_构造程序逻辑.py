33d9b358fe900b62e0e3bbcfb65a8237672f0b64
rama <ramastarzhu01@gmail.com>
2023-09-22 16:07:32 +0800

    initialization finished

---
+++ exercise/01_构造程序逻辑.py
@@ -0,0 +1,25 @@
+# 1.水仙花数
+# for num in range(100, 1000):
+#     a = num // 100
+#     b = num // 10 % 10
+#     c = num % 10
+#     if num == a ** 3 + b ** 3 + c ** 3:
+#         print(num)
+
+# 2.反转一个正整数
+# num = int(input('num = '))
+# reversed_num = 0
+# while num > 0:
+#     reversed_num = reversed_num * 10 + num % 10
+#     # 去除前两位数字，以及吧百位数
+#     num //= 10
+# print(reversed_num)
+
+# 3.百钱百鸡问题
+# for x in range(0, 20):
+#     for y in range(0, 33):
+#         z = 100 - x - y
+#         if 5 * x + 3 * y + z / 3 == 100:
+#             print('公鸡:%d,母鸡:%d,小鸡:%d' % (x, y, z))
+
+# 4.
---
+++ exercise/02_函数和模块的使用.py
@@ -0,0 +1,60 @@
+"""
+输入M和N计算C(M,N)
+Version: 0.1
+Author: 骆昊
+"""
+
+
+# 1.计算C(m,n)
+# m = int(input('m='))
+# n = int(input('n='))
+# # 计算m!
+# fm = 1
+# for num in range(1, m + 1):
+#     fm *= num
+# fn = 1
+# for num in range(1, n + 1):
+#     fn *= num
+# fmn = 1
+# for num in range(1, m - n + 1):
+#     fmn *= num
+# print(fm // fn // fmn)
+
+
+# 2.代码重构
+# def factorial(num):
+#     res = 1
+#     for i in range(1, num + 1):
+#         res *= i
+#     return res
+#
+#
+# m = int(input('m='))
+# n = int(input('n='))
+# print(factorial(m) // factorial(n) // factorial(m - n))
+
+# 函数参数
+# 3.投掷骰子
+# from random import randint
+#
+#
+# def roll_dice(n=2):
+#     """摇色子"""
+#     total = 0
+#     for _ in range(n):
+#         total += randint(1, 6)
+#     return total
+#
+#
+# print(roll_dice(6))
+# print(roll_dice(3))
+
+# 4.可变参数
+def add(*args):
+    total = 0
+    for num in args:
+        total += num
+    return total
+
+
+print(add(1, 2, 3))
