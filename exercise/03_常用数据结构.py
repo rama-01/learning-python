2e1d610d5a8bef9c92cc3f877bb7a29eceee10f1
rama <ramastarzhu01@gmail.com>
2023-09-23 19:24:17 +0800

    continue learning

--- exercise/03_常用数据结构.py
+++ exercise/03_常用数据结构.py
@@ -93,19 +93,50 @@
 # 练习2 生成验证码
 import random
 
-def generate_code(code_len=4):
-    """
-    生成指定长度的验证码
-    :param code_len: 验证码的长度(默认4个字符)
-    :return: 由大小写英文字母和数字构成的随机验证码
-    """
-    all_chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    last_pos = len(all_chars) - 1
-    code = ''
-    for _ in range(code_len):
-        index = random.randint(0, last_pos)
-        code += all_chars[index]
-    return code
+# def generate_code(code_len=4):
+#     """
+#     生成指定长度的验证码
+#     :param code_len: 验证码的长度(默认4个字符)
+#     :return: 由大小写英文字母和数字构成的随机验证码
+#     """
+#     all_chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+#     last_pos = len(all_chars) - 1
+#     code = ''
+#     for _ in range(code_len):
+#         index = random.randint(0, last_pos)
+#         code += all_chars[index]
+#     return code
+#
+# code = generate_code()
+# print(code)
 
-code = generate_code()
-print(code)
+# 练习3 返回文件的后缀名
+# def get_suffix(filename, has_dot=False):
+#     """
+#     获取文件名的后缀名
+#     :param filename: 文件名
+#     :param has_dot: 返回的后缀名是否需要带点
+#     :return: 文件的后缀名
+#     """
+#     pos = filename.rfind('.')
+#     if 0 < pos < len(filename) - 1:
+#         index = pos if has_dot else pos + 1
+#         return filename[index:]
+#     else:
+#         return ''
+#
+# print(get_suffix('learning-center.py'))
+
+# 练习4 设计一个函数返回传入列表的最大和第二大的元素
+# def max2(x):
+#     m1, m2 = (x[0], x[1]) if x[0] > x[1] else (x[1], x[0])
+#     for index in range(2, len(x)):
+#         if x[index] > m1:
+#             m2 = m1
+#             m1 = x[index]
+#         elif x[index] > m2:
+#             m2 = x[index]
+#     return m1, m2
+# # 这里无需考虑x[index] <m1,m2的情况，如果是这样，最后结果是直接返回m1,m2
+# print(max2([1,2,12,-12]))
+
